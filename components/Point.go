fc0df1df114ce56b2fda5c92726bb5f794750a01
poixen <githubpoixen@gmail.com>
2016-06-18 20:46:14 +0200

    Fixed bug with not correctly calculating neighbors

--- components/Point.go
+++ components/Point.go
@@ -8,10 +8,23 @@ func NewPoint(x, y int) *Point {
 	return &Point{x, y}
 }
 
-func (p *Point) Neighbors() []*Point {
-	return []*Point{
+func (p *Point) Neighbors(w, h int) []*Point {
+
+	// default neighbors
+	neighbors := []*Point{
 		NewPoint(p.x - 1, p.y - 1), NewPoint(p.x, p.y - 1), NewPoint(p.x + 1, p.y - 1),
 		NewPoint(p.x - 1, p.y), 			    NewPoint(p.x + 1, p.y),
 		NewPoint(p.x - 1, p.y + 1), NewPoint(p.x , p.y + 1), NewPoint(p.x + 1, p.y + 1),
 	}
+
+	// collect all valid neighbors
+	validNeighbors := []*Point{}
+	for _, neighbor := range neighbors {
+		if (neighbor.x >= 0) && (neighbor.x < w) &&
+		(neighbor.y >= 0) && (neighbor.y < h) {
+			validNeighbors = append(validNeighbors, neighbor)
+		}
+	}
+
+	return validNeighbors
 }
